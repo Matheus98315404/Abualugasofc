d52089f2edd4043fde22cae4d484275cf94c2891
Wellison Ferreira <146448566+Uelli@users.noreply.github.com>
2024-07-12 18:29:53 -0300

    atulização da pagina de realização de aluguel

--- code/pagamento.php
+++ code/pagamento.php
@@ -1,30 +1,29 @@
 <?php
 require_once 'conexao.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_funcionario = $_POST['id_funcionario'];
     $id_cliente = $_POST['id_cliente'];
     $id_veiculo = $_POST['id_veiculo'];
-    $data_aluguel = $_POST['data_aluguel'];
+    $data_inicio = $_POST['data_aluguel'];
     $km_inicial = $_POST['km_inicial'];
 
-    
-    // Calcula o valor do aluguel baseado na quilometragem rodada
-    //$km_rodado = $km_final - $km_inicial;
-    //$valor = $km_rodado * 1; // R$1 por km rodado
+      
+    $sql = "INSERT INTO alugueis (id_funcionario, id_cliente, id_veiculo, data_inicio, km_inicial)
+    VALUES ('$id_funcionario', '$id_cliente', '$id_veiculo', '$data_inicio', '$km_inicial')";
 
-    // Insere o aluguel no banco de dados
-    $sql = "INSERT INTO alugueis (id_funcionario, id_cliente, id_veiculo, data_aluguel)
-            VALUES ('$id_funcionario', '$id_cliente', '$id_veiculo', '$data_aluguel', )";
-    
+
     if (mysqli_query($conexao, $sql)) {
         // Atualiza a quilometragem do veículo e a disponibilidade
         $sql_update = "UPDATE veiculos SET km_atual = '$km_inicial', disponivel = 0 WHERE id_veiculo = '$id_veiculo'";
+
         if (mysqli_query($conexao, $sql_update)) {
-        
-        echo "Aluguel realizado com sucesso!";
+            //echo "Aluguel realizado com sucesso!";
+        } else {
+            echo "Erro ao atualizar veículo: " . mysqli_error($conexao);
+        }
     } else {
-        echo "Erro: " . $sql . "<br>" . mysqli_error($conexao);
+        echo "Erro ao inserir aluguel: " . mysqli_error($conexao);
     }
 
     $conexao->close();
@@ -36,11 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Pagamento</title>
+<title>aluguel</title>
 </head>
 <body>
-    <h2>Pagamento</h2>
-    <p>O pagamento do valor de R$<?php echo $valor; ?> foi realizado com sucesso.</p>
+    <h2>Realização do aluguel</h2>
+    <p>O aluguel foi realizado com sucesso!</p>
     <a href="index.html">Voltar à página inicial</a>
 </body>
 </html>
