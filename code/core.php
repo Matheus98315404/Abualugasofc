b3f6aa4208e99deb0cbe1c87467e021fea7c53b6
Felipinr <felipematospl67889@gmail.com>
2024-08-01 16:22:10 -0300

    atualizando o core.php
    adicionando funções novas

--- code/core.php
+++ code/core.php
@@ -1,8 +1,57 @@
-<?php 
+<?php
 
-function trocarIdporNome($conexao, $id)
+function listarAlunos($conexao)
 {
-    $sql = "SELECT nome FROM tb_xxxxxx WHERE id = ?";
+    $sql = "SELECT * FROM tb_aluno";
+
+    $stmt = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_bind_result($stmt, $id, $idSituacao, $nome, $dataCadastro);
+
+    mysqli_stmt_store_result($stmt);
+
+    $lista = [];
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        while (mysqli_stmt_fetch($stmt)) {
+            $situacao = buscarNomeSituacaoPorId($conexao, $idSituacao);
+
+            $lista[] = [$id, $situacao, $nome, $dataCadastro];
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+
+    return $lista;
+}
+
+function listarSituacoes($conexao)
+{
+    $sql = "SELECT * FROM tb_situacao";
+
+    $stmt = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_bind_result($stmt, $id, $nome);
+
+    mysqli_stmt_store_result($stmt);
+
+    $lista = [];
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        while (mysqli_stmt_fetch($stmt)) {
+            $lista[] = [$id, $nome];
+        }
+    }
+    mysqli_stmt_close($stmt);
+
+    return $lista;
+}
+
+function buscarNomeSituacaoPorId($conexao, $id)
+{
+    $sql = "SELECT nome FROM tb_situacao WHERE id = ?";
 
     $stmt = mysqli_prepare($conexao, $sql);
 
@@ -15,7 +64,6 @@ function trocarIdporNome($conexao, $id)
     if (mysqli_stmt_fetch($stmt)) {
         return $nome;
     }
- 
-    mysqli_stmt_close($stmt);
 
+    mysqli_stmt_close($stmt);
 }
