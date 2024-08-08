5139382fcc6db1f3bf52f483abb6398d8cfa2748
Uelli <wellison.fdsilva@gmail.com>
2024-08-08 16:47:01 -0300

    criação do listar veiculos

--- code/core.php
+++ code/core.php
@@ -2,68 +2,68 @@
 
 function listarCarros($conexao)
 {
-    $sql = "SELECT * FROM tb_aluno";
+    $sql = "SELECT * FROM veiculos";
 
     $stmt = mysqli_prepare($conexao, $sql);
 
     mysqli_stmt_execute($stmt);
 
-    mysqli_stmt_bind_result($stmt, $id, $idSituacao, $nome, $dataCadastro);
+    mysqli_stmt_bind_result($stmt, $id_veiculo, $modelo, $marca, $ano, $placa, $cor, $km_atual, $tipo, $airbag, $num_bancos, $num_portas, $combustivel, $cambio, $ar_coondicionado, $direcao, $som, $bluetooth, $gps, $sensor_estacionamento, $camera_re, $disponivel);
 
     mysqli_stmt_store_result($stmt);
 
     $lista = [];
     if (mysqli_stmt_num_rows($stmt) > 0) {
         while (mysqli_stmt_fetch($stmt)) {
-            $situacao = buscarNomeSituacaoPorId($conexao, $idSituacao);
+            // $situacao = buscarNomeSituacaoPorId($conexao, $id_veiculo);
 
-            $lista[] = [$id, $situacao, $nome, $dataCadastro];
+            $lista[] = [$id_veiculo, $modelo, $marca, $ano, $placa, $cor, $km_atual, $tipo, $airbag, $num_bancos, $num_portas, $combustivel, $cambio, $ar_coondicionado, $direcao, $som, $bluetooth, $gps, $sensor_estacionamento, $camera_re, $disponivel];
         }
     }
 
     mysqli_stmt_close($stmt);
-
+    // echo $lista;
     return $lista;
 }
 
-function listarSituacoes($conexao)
-{
-    $sql = "SELECT * FROM tb_situacao";
+// function listarSituacoes($conexao)
+// {
+//    $sql = "SELECT * FROM veiculos";
+//
+//    $stmt = mysqli_prepare($conexao, $sql);
+//
+//  mysqli_stmt_execute($stmt);
+//
+//    mysqli_stmt_bind_result($stmt, $id_veiculo, $modelo);
+//
+//    mysqli_stmt_store_result($stmt);
+//
+//    $lista = [];
+//    if (mysqli_stmt_num_rows($stmt) > 0) {
+//        while (mysqli_stmt_fetch($stmt)) {
+//            $lista[] = [$id_veiculo, $modelo];
+//        }
+//    }
+//    mysqli_stmt_close($stmt);
+//
+//   return $lista;
+//}
 
-    $stmt = mysqli_prepare($conexao, $sql);
+//function buscarNomeSituacaoPorId($conexao, $id_veiculo)
+//{
+//     $sql = "SELECT id_veiculo FROM veiculos WHERE id_veiculo = ?";
 
-    mysqli_stmt_execute($stmt);
+//     $stmt = mysqli_prepare($conexao, $sql);
 
-    mysqli_stmt_bind_result($stmt, $id, $nome);
+//     mysqli_stmt_bind_param($stmt, "i", $id_veiculo);
 
-    mysqli_stmt_store_result($stmt);
+//     mysqli_stmt_execute($stmt);
 
-    $lista = [];
-    if (mysqli_stmt_num_rows($stmt) > 0) {
-        while (mysqli_stmt_fetch($stmt)) {
-            $lista[] = [$id, $nome];
-        }
-    }
-    mysqli_stmt_close($stmt);
+//     mysqli_stmt_bind_result($stmt, $modelo);
 
-    return $lista;
-}
+//     if (mysqli_stmt_fetch($stmt)) {
+//         return $modelo;
+//     }
 
-function buscarNomeSituacaoPorId($conexao, $id)
-{
-    $sql = "SELECT nome FROM tb_situacao WHERE id = ?";
-
-    $stmt = mysqli_prepare($conexao, $sql);
-
-    mysqli_stmt_bind_param($stmt, "i", $id);
-
-    mysqli_stmt_execute($stmt);
-
-    mysqli_stmt_bind_result($stmt, $nome);
-
-    if (mysqli_stmt_fetch($stmt)) {
-        return $nome;
-    }
-
-    mysqli_stmt_close($stmt);
-}
+//     mysqli_stmt_close($stmt);
+// }
--- code/listar_carros.php
+++ code/listar_carros.php
@@ -4,37 +4,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de carros</title>
+    <title>Lista de Carros - Bootstrap</title>
+    <!-- Incluindo o Bootstrap CSS via CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Lista de carros</h1>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Lista de Carros</h1>
 
-    <table style="border-style: solid;">
-        <tr>
-            <td>modelo</td>
-            <td>marca</td>
-            <td>placa</td>
-            <td>cor</td>
-        </tr>
-        <?php
-        require_once "conexao.php";
-        require_once "core.php";
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Id do veiculo</th>
+                    <th>Modelo</th>
+                    <th>Marca</th>
+                    <th>Ano</th>
+                    <th>Placa</th>
+                    <th>Cor</th>
+                    <th>Km atual</th>
+                    <th>Tipo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once "conexao.php";
+                require_once "core.php";
 
-        $resultados = listarCarros($conexao);
+                $resultados = listarCarros($conexao);
 
-        foreach ($resultados as $modelo) {
-            echo "<tr>";
-            echo "<td>$modelo[0]</td>";
-            echo "<td>$marca[1]</td>";
-            echo "<td>$placa[2]</td>";
-            echo "<td>$cor[3]</td>";
-            echo "</tr>";
-        }
-        ?>
-    </table>
+                foreach ($resultados as $modelo) {
+                    echo "<tr>";
+                    echo "<td>$modelo[0]</td>";
+                    echo "<td>$modelo[1]</td>";
+                    echo "<td>$modelo[2]</td>";
+                    echo "<td>$modelo[3]</td>";
+                    echo "<td>$modelo[4]</td>";
+                    echo "<td>$modelo[5]</td>";
+                    echo "<td>$modelo[6]</td>";
+                    echo "<td>$modelo[7]</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
 
-    <br><a href="index.html"><< Página inicial</a>
+        <div class="text-center mt-4">
+            <a href="index.html" class="btn btn-primary"><< Página inicial</a><br><br>
+        </div>
+    </div>
+
+    <!-- Incluindo o Bootstrap JS e dependências via CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
