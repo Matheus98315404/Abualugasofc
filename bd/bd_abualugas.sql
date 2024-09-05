a49828e76beb3ef7d391a3910ca0c037db0236f3
Felipinr <felipematospl67889@gmail.com>
2024-09-05 14:51:42 -0300

    Alteração bd
    Alteração bd

--- bd/bd_abualugas.sql
+++ bd/bd_abualugas.sql
@@ -17,7 +17,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema bd_abualugas
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `bd_abualugas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+
+CREATE DATABASE bd_abualugas;
+
+CREATE SCHEMA IF NOT EXISTS `bd_abualugas` DEFAULT CHARACTER SET utf8mb4;
 USE `bd_abualugas` ;
 
 -- -----------------------------------------------------
