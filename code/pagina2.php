1b47303e43e2512c1fd1d97f0b0c0cddefdd1d3f
Uelli <wellison.fdsilva@gmail.com>
2024-10-28 08:46:04 -0300

    Update pagina2.php

--- code/pagina2.php
+++ code/pagina2.php
@@ -84,38 +84,46 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Carromeu e julieta</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Carromeu e julieta</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+            </ul>
         </div>
-    </nav>
-    <div class="container form-container mt-4">
-        <h2>Selecione os Veículos</h2>
-        <form action="pagina3.php" method="POST">
-            <input type="hidden" name="id_funcionario" value="<?php echo $_POST['id_funcionario']; ?>">
-            <input type="hidden" name="id_cliente" value="<?php echo $_POST['id_cliente']; ?>">
-            <?php
-            require_once 'conexao.php';
-            $query_veiculos = "SELECT id_veiculo, placa FROM veiculos WHERE disponivel = 1";
-            $result_veiculos = mysqli_query($conexao, $query_veiculos);
-            while ($row = mysqli_fetch_assoc($result_veiculos)) {
-                echo "<label><input type='checkbox' name='veiculos[]' value='{$row['id_veiculo']}'> {$row['placa']}</label><br>";
-            }
-            ?>
-            <button type="submit" class="btn btn-custom mt-3">Próxima Etapa</button>
-        </form>
     </div>
-    <footer class="footer text-center">
+</nav>
+<div class="container form-container mt-4">
+    <h2>Selecione os Veículos</h2>
+    <form action="pagina3.php" method="POST">
+        <!-- Campo oculto para o ID do funcionário -->
+        <input type="hidden" name="id_funcionario" value="<?php echo $_POST['id_funcionario']; ?>">
+        <!-- Campo oculto para o ID do cliente -->
+        <input type="hidden" name="id_cliente" value="<?php echo $_POST['id_cliente']; ?>">
+        <?php
+        // Inclui a conexão com o banco de dados
+        require_once 'conexao.php';
+
+        // Consulta os veículos disponíveis
+        $query_veiculos = "SELECT id_veiculo, placa FROM veiculos WHERE disponivel = 1";
+        $result_veiculos = mysqli_query($conexao, $query_veiculos);
+
+        // Exibe os veículos disponíveis em forma de checkboxes
+        while ($row = mysqli_fetch_assoc($result_veiculos)) {
+            echo "<label><input type='checkbox' name='veiculos[]' value='{$row['id_veiculo']}'> {$row['placa']}</label><br>";
+        }
+        ?>
+        <!-- Botão para enviar o formulário e passar para a próxima etapa -->
+        <button type="submit" class="btn btn-custom mt-3">Próxima Etapa</button>
+    </form>
+</div>
+<footer class="footer text-center">
     <p>© 2024 Carromeu e Julieta - Todos os direitos reservados</p>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
